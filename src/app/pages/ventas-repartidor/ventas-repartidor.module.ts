71d06e642bc7bd7bb5607f2e0ac70c45f754c849
Yael <yaelsilvestre64@gmail.com>
2025-04-01 17:28:06 -0600

    Hola

--- src/app/pages/ventas-repartidor/ventas-repartidor.module.ts
+++ src/app/pages/ventas-repartidor/ventas-repartidor.module.ts
@@ -5,7 +5,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 
 import { VentasRepartidorPageRoutingModule } from './ventas-repartidor-routing.module';
-import { HeaderComponent } from 'src/app/components/header/header.component';
+
 
 
 
